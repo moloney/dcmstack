847653f90b56955dda959295e4fbe803b56d6646
Brendan Moloney <moloney.brendan@gmail.com>
2012-05-10 17:33:29 -0700

    TST+RF: Added tests for extracting private CSA headers. Improved the code to allow all test cases to pass.

--- src/dcmstack/extract.py
+++ src/dcmstack/extract.py
@@ -66,8 +66,9 @@ def simplify_csa_dict(csa_dict):
     return result
 
 def csa_image_trans_func(elem):
+    '''Function for translating the CSA image sub header.'''
     return simplify_csa_dict(csareader.read(elem.value))
-'''Function for translating the CSA image sub header.'''
+
     
 csa_image_trans = Translator('CsaImage', 
                              dicom.tag.Tag(0x29, 0x1010), 
@@ -75,23 +76,51 @@ csa_image_trans = Translator('CsaImage',
                              csa_image_trans_func)
 '''Translator for the CSA image sub header.'''
 
-def _parse_phoenix_line(line):
-    #Handle comments at end of lines
-    comment_idx = line.find('#')   
-    if comment_idx != -1:
-        line = line[:comment_idx]
+class PhoenixParseError(Exception):
+    '''Exception indicating a error parsing a line from the Phoenix Protocol.
+    '''
+    def __init__(self, line):
+        self.line = line
         
-    #Split on the equals sign and strip the tokens
-    tokens = [token.strip() for token in line.split('=')]
-    if len(tokens) != 2:
+    def __str__(self):
+        return 'Unable to parse phoenix protocol line: %s' % self.line
+
+def _parse_phoenix_line(line, str_delim='""'):
+    delim_len = len(str_delim)
+    #Handle most comments (not always when string literal involved)
+    comment_idx = line.find('#')
+    if comment_idx != -1:
+        #Check if the pound sign is in a string literal
+        if line[:comment_idx].count(str_delim) == 1:
+            if line[comment_idx:].find(str_delim) == -1:
+                raise PhoenixParseError(line)
+        else:
+            line = line[:comment_idx]
+         
+    #Allow empty lines
+    if line.strip() == '':
         return None
-   
-    #Convert the value to a string, integer, or float
-    key = tokens[0]
-    val_str = tokens[1]
-    if val_str.startswith('""') and val_str.endswith('""'):
-        return (key, val_str.strip('""'))
-    else:
+    
+    #Find the first equals sign and use that to split key/value
+    equals_idx = line.find('=')
+    if equals_idx == -1:
+        raise PhoenixParseError(line)
+    key = line[:equals_idx].strip()
+    val_str = line[equals_idx + 1:].strip()
+    
+    #If there is a string literal, pull that out
+    if val_str.startswith(str_delim):
+        end_quote = val_str[delim_len:].find(str_delim) + delim_len
+        if end_quote == -1:
+            raise PhoenixParseError(line)
+        elif not end_quote == len(val_str) - delim_len:
+            #Make sure remainder is just comment
+            if not val_str[end_quote+delim_len:].strip().startswith('#'):
+                raise PhoenixParseError(line)
+            
+        return (key, val_str[2:end_quote])
+        
+    else: #Otherwise try to convert to an int or float
         val = None
         try:
             val = int(val_str)
@@ -114,28 +143,38 @@ def _parse_phoenix_line(line):
         else:
             return (key, val)
     
-        raise ValueError('Unable to parse phoenix protocol line: %s' % line)
+    raise PhoenixParseError(line)
 
-def parse_phoenix_prot(prot_str):
+def parse_phoenix_prot(prot_key, prot_val):
     '''Parse the MrPheonixProtocol string.
 
     Parameters
     ----------
     prot_str : str
-        The MrPheonixProtocol string.
+        The 'MrPheonixProtocol' string from the CSA Series sub header.
         
     Returns
     -------
-    prot_dict : dict
+    prot_dict : OrderedDict
         Meta data pulled from the ASCCONV section.
+        
+    Raises
+    ------
+    PhoenixParseError : A line of the ASCCONV section could not be parsed.
     '''
-    ascconv_start = prot_str.find('### ASCCONV BEGIN ###')
-    ascconv_end = prot_str.find('### ASCCONV END ###')
-    ascconv = prot_str[ascconv_start:ascconv_end].split('\n')[1:-1]
+    if prot_key == 'MrPhoenixProtocol':
+        str_delim = '""'
+    elif prot_key == 'MrProtocol':
+        str_delim = '"'
+    else:
+        raise ValueError('Unknown protocol key: %s' % prot_key)
+    ascconv_start = prot_val.find('### ASCCONV BEGIN ###')
+    ascconv_end = prot_val.find('### ASCCONV END ###')
+    ascconv = prot_val[ascconv_start:ascconv_end].split('\n')[1:-1]
     
     result = OrderedDict()    
     for line in ascconv:
-        parse_result = _parse_phoenix_line(line)
+        parse_result = _parse_phoenix_line(line, str_delim)
         if parse_result:
             result[parse_result[0]] = parse_result[1]
         
@@ -146,10 +185,15 @@ def csa_series_trans_func(elem):
     csa_dict = simplify_csa_dict(csareader.read(elem.value))
     
     #If there is a phoenix protocol, parse it and dump it into the csa_dict
+    phx_src = None
     if 'MrPhoenixProtocol' in csa_dict:
-        phoenix_dict = parse_phoenix_prot(csa_dict['MrPhoenixProtocol'])
-        del csa_dict['MrPhoenixProtocol']
-        
+        phx_src = 'MrPhoenixProtocol'
+    elif 'MrProtocol' in csa_dict:
+        phx_src = 'MrProtocol'
+    print phx_src
+    if not phx_src is None:
+        phoenix_dict = parse_phoenix_prot(phx_src, csa_dict[phx_src])
+        del csa_dict[phx_src]
         for key, val in phoenix_dict.iteritems():
             new_key = '%s.%s' % ('MrPhoenixProtocol', key)
             csa_dict[new_key] = val
---
+++ test/test_extract.py
@@ -0,0 +1,59 @@
+"""
+Tests for dcmstack.extract
+"""
+import sys
+sys.path.insert(0, '../src')
+from nose.tools import ok_, eq_, assert_raises
+import dicom
+from nibabel.nicom import csareader
+from dcmstack import extract
+
+class TestCsa(object):
+    def setUp(self):
+        data_fn = './data/extract/csa_test.dcm'
+        self.data = dicom.read_file(data_fn)
+        
+    def tearDown(self):
+        del self.data
+        
+    def test_simplify(self):
+        eq_(extract.simplify_csa_dict(None), None)
+        csa_dict = csareader.read(self.data[dicom.tag.Tag(0x29, 0x1010)].value)
+        simp_dict = extract.simplify_csa_dict(csa_dict)
+        for tag in csa_dict['tags']:
+            items = csa_dict['tags'][tag]['items']
+            if len(items) == 0:
+                ok_(not tag in simp_dict)
+            elif len(items) == 1:
+                eq_(simp_dict[tag], items[0])
+            else:
+                eq_(simp_dict[tag], items)
+                
+    def test_parse_phx_line(self):
+        ok_(extract._parse_phoenix_line("") is None)
+        ok_(extract._parse_phoenix_line("#test = 2") is None)
+        
+        eq_(extract._parse_phoenix_line('test = "" 2 ""'), ('test', ' 2 '))
+        eq_(extract._parse_phoenix_line('test = "" #2 ""'), ('test', ' #2 '))
+        eq_(extract._parse_phoenix_line('test = "" 2 ""#='), 
+            ('test', ' 2 '))
+        eq_(extract._parse_phoenix_line("test = 2#"), ('test', 2))
+        eq_(extract._parse_phoenix_line("test = 0x2"), ('test', 2))
+        eq_(extract._parse_phoenix_line("test = 2."), ('test', 2.0))
+        
+        assert_raises(extract.PhoenixParseError, 
+                      extract._parse_phoenix_line, 
+                      'test = blah')
+        assert_raises(extract.PhoenixParseError, 
+                      extract._parse_phoenix_line, 
+                      '==')
+        assert_raises(extract.PhoenixParseError, 
+                      extract._parse_phoenix_line, 
+                      'test')
+        assert_raises(extract.PhoenixParseError, 
+                      extract._parse_phoenix_line, 
+                      'test = "" 2 ""3')
+        
+    def test_csa_series_trans(self): 
+        csa_dict = extract.csa_series_trans_func(self.data[(0x29, 0x1020)])
+        eq_(csa_dict['MrPhoenixProtocol.sEFISPEC.bEFIDataValid'], 1)
