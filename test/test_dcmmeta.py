16c510c160de9d562763977a778e0cb5ad4d6ff3
Brendan Moloney <moloney.brendan@gmail.com>
2012-08-31 17:03:00 -0700

    TST: Added tests for DcmMetaExtension.check_valid

--- test/test_dcmmeta.py
+++ test/test_dcmmeta.py
@@ -70,3 +70,79 @@ def test_get_valid_classes():
          ('vector', 'slices')
         )
        )
+
+class TestCheckValid(object):
+    def setUp(self):
+        self.ext = dcmmeta.DcmMetaExtension.make_empty((64, 64, 2, 3, 4), 
+                                                       np.eye(4), 
+                                                       2)
+    
+    def test_empty(self):
+        self.ext.check_valid()
+                                               
+    def test_const(self):
+        self.ext.get_class_dict(('global', 'const'))['ConstTest'] = 2
+        self.ext.check_valid()
+        del self.ext._content['global']['const']
+        assert_raises(dcmmeta.InvalidExtensionError, self.ext.check_valid)
+        
+    def test_global_slices(self):
+        cls_dict = self.ext.get_class_dict(('global', 'slices'))
+        cls_dict['SliceTest'] = [0] * 23
+        assert_raises(dcmmeta.InvalidExtensionError, self.ext.check_valid)
+        cls_dict['SliceTest'] = [0] * 24
+        self.ext.check_valid()
+        del self.ext._content['global']['slices']
+        assert_raises(dcmmeta.InvalidExtensionError, self.ext.check_valid)
+        
+    def test_time_samples(self):
+        cls_dict = self.ext.get_class_dict(('time', 'samples'))
+        cls_dict['TimeSampleTest'] = [0] * 2
+        assert_raises(dcmmeta.InvalidExtensionError, self.ext.check_valid)
+        cls_dict['TimeSampleTest'] = [0] * 3
+        self.ext.check_valid()
+        del self.ext._content['time']['samples']
+        assert_raises(dcmmeta.InvalidExtensionError, self.ext.check_valid)
+        
+    def test_time_slices(self):
+        cls_dict = self.ext.get_class_dict(('time', 'slices'))
+        cls_dict['TimeSliceTest'] = [0] 
+        assert_raises(dcmmeta.InvalidExtensionError, self.ext.check_valid)
+        cls_dict['TimeSliceTest'] = [0] * 2
+        self.ext.check_valid()
+        del self.ext._content['time']['slices']
+        assert_raises(dcmmeta.InvalidExtensionError, self.ext.check_valid)
+        
+    def test_vector_samples(self):
+        cls_dict = self.ext.get_class_dict(('vector', 'samples'))
+        cls_dict['VectorSampleTest'] = [0] * 3
+        assert_raises(dcmmeta.InvalidExtensionError, self.ext.check_valid)
+        cls_dict['VectorSampleTest'] = [0] * 4
+        self.ext.check_valid()
+        del self.ext._content['vector']['samples']
+        assert_raises(dcmmeta.InvalidExtensionError, self.ext.check_valid)
+        
+    def test_vector_slices(self):
+        cls_dict = self.ext.get_class_dict(('vector', 'slices'))
+        cls_dict['VectorSliceTest'] = [0] * 5
+        assert_raises(dcmmeta.InvalidExtensionError, self.ext.check_valid)
+        cls_dict['VectorSliceTest'] = [0] * 6
+        self.ext.check_valid()
+        del self.ext._content['vector']['slices']
+        assert_raises(dcmmeta.InvalidExtensionError, self.ext.check_valid)
+        
+    def test_invalid_affine(self):
+        self.ext.set_affine(np.eye(3))
+        assert_raises(dcmmeta.InvalidExtensionError, self.ext.check_valid)
+        
+    def test_invalid_slice_dim(self):
+        self.ext.set_slice_dim(3)
+        assert_raises(dcmmeta.InvalidExtensionError, self.ext.check_valid)
+        self.ext.set_slice_dim(-1)
+        assert_raises(dcmmeta.InvalidExtensionError, self.ext.check_valid)
+        
+    def test_invalid_shape(self):
+        self.ext.set_shape((2, 2))
+        assert_raises(dcmmeta.InvalidExtensionError, self.ext.check_valid)
+        self.ext.set_shape((2, 2, 1, 1, 1, 2))
+        assert_raises(dcmmeta.InvalidExtensionError, self.ext.check_valid)
