1d45570adb11d321857d6c0223cea4aeaa25d9f2
Brendan Moloney <moloney.brendan@gmail.com>
2012-11-15 17:56:23 -0800

    DOC: Still trying to get RTD to work...

--- doc/conf.py
+++ doc/conf.py
@@ -13,6 +13,13 @@
 
 import sys, os
 
+#Mock unavailable packages for ReadTheDocs
+import mock
+ 
+MOCK_MODULES = ['numpy', 'nibabel']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
