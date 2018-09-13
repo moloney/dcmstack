a7da2b11c6c08b8e1bb8d591445efa29ebd5921f
Brendan Moloney <moloney@ohsu.edu>
2018-09-12 20:28:14 -0700

    BF: Include str_types definition needed by previous commit

--- src/dcmstack/utils.py
+++ src/dcmstack/utils.py
@@ -9,5 +9,7 @@ import sys, string
 PY2 = sys.version_info[0] < 3
 
 unicode_str = unicode if PY2 else str
+byte_str = str if PY2 else bytes
+str_types = (unicode_str, byte_str)
 iteritems = (lambda d: d.iteritems()) if PY2 else lambda d: d.items()
 ascii_letters = string.letters if PY2 else string.ascii_letters
