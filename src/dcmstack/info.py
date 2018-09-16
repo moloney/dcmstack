ea91087a227f84be8ef189b6869982c378e14a8d
Brendan Moloney <moloney@ohsu.edu>
2018-09-16 15:51:33 -0700

    REL: Cut 0.7 release

--- src/dcmstack/info.py
+++ src/dcmstack/info.py
@@ -6,7 +6,7 @@ import sys
 _version_major = 0
 _version_minor = 7
 _version_micro = 0
-_version_extra = 'dev'
+_version_extra = ''
 __version__ = "%s.%s.%s%s" % (_version_major,
                               _version_minor,
                               _version_micro,
