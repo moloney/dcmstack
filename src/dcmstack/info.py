28fa6d3d52ace6941e5cd55a43be6dddc745ffdc
Brendan Moloney <moloney@ohsu.edu>
2022-09-23 17:15:30 -0700

    ENH: Add support for JPEG encoded DICOM

--- src/dcmstack/info.py
+++ src/dcmstack/info.py
@@ -25,6 +25,7 @@ description = 'Stack DICOM images into volumes and convert to Nifti'
 # Hard dependencies
 install_requires = ['pydicom >= 0.9.7',
                     'nibabel >= 2.5.1',
+                    'pylibjpeg-libjpeg',
                    ]
 
 # Extra requirements for building documentation and testing
